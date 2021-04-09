ef4f0c241c54d1f2f0f32b4fdd92e9237ac67202
Nick Whitelegg <nickw4426@gmail.com>
2021-04-09 16:58:37 +0100

    correct database.sql in basic example

--- basic/database.sql
+++ basic/database.sql
@@ -1,4 +1,9 @@
-CREATE TABLE panoramas(id serial, the_geom geometry, ele FLOAT, pan FLOAT DEFAULT 0, tilt FLOAT DEFAULT 0, roll FLOAT DEFAULT 0 authorised INT DEFAULT 0, userid INT DEFAULT 0, timestamp INT);
+DROP TABLE IF EXISTS panoramas;
+DROP TABLE IF EXISTS users; 
+DROP TABLE IF EXISTS sequence_geom; 
+DROP TABLE IF EXISTS sequence_panos; 
+
+CREATE TABLE panoramas(id serial, the_geom geometry, ele FLOAT, pancorrection FLOAT DEFAULT 0, tiltcorrection FLOAT DEFAULT 0, rollcorrection FLOAT DEFAULT 0, poseheadingdegrees FLOAT DEFAULT 0, authorised INT DEFAULT 0, userid INT DEFAULT 0, timestamp INT);
 CREATE TABLE users (id serial, username VARCHAR(255), password VARCHAR(255), isadmin INT DEFAULT 0);
 CREATE TABLE sequence_geom(id serial, the_geom geometry);
 CREATE TABLE sequence_panos(id serial, sequenceid INT, panoid INT);
